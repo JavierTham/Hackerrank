e1b3f03d5c3bf0cfad46f5df2e6b7e636050b780
Javier Tham <javiertjl99@gmail.com>
2021-12-17 16:16:07 +0800

    completed code

--- Day12/TreeTopView.py
+++ Day12/TreeTopView.py
@@ -3,7 +3,8 @@ class Node:
         self.info = info  
         self.left = None  
         self.right = None 
-        self.level = None 
+        self.dist = None # to store horizontal distance from center. 
+                         # root.dist = 0, root.left.dist = -1, root.right.dist = 1
 
     def __str__(self):
         return str(self.info) 
@@ -35,22 +36,56 @@ class BinarySearchTree:
                     break
 
 def topView(root):
-    #Write your code here
-    def helper(root, direction):
-        if not root:
-            return ""
-        if direction == "left":
-            print(root.info, end = " ")
-            helper(root.left, "left")
-            
-        if direction == "right":
-            print(root.info, end = " ")
-            helper(root.right, "right")
-    
-    helper(root.left, "left")
-    print(root.info, end = " ")
-    helper(root.right, "right")
+    # do a level order traversal down the tree
+    # take note of horizontal distance from center
+
+    if not root:
+        return 
+
+    # queue for level order traversal, child nodes are put into the queue
+    queue = []
+    root.dist = 0
+    queue.append(root)
+
+    # dictionary to store first node at that horizontal distance
+    # key - horizontal distance from center
+    # value - node.info
+    top_nodes_dist = {}
+
+    # keep track of distance of node that is furthest left
+    max_left_dist = 0
+
+    # level order traversal, keep going down tree
+    while(queue):
+        curr_node = queue.pop(0)
+
+        # first time a node reaches this distance from center (this node can be viewed from the top)
+        if curr_node.dist not in top_nodes_dist:
+            top_nodes_dist[curr_node.dist] = curr_node.info
+            max_left_dist = min(max_left_dist, curr_node.dist)
+
+        # already have another node at this distance (this node cannot be viewed from top)
+        # if have left child
+        if curr_node.left:
+            # update child's distance from center
+            curr_node.left.dist = curr_node.dist - 1
+            queue.append(curr_node.left)
         
+        # if have right child
+        if curr_node.right:
+            # update child's distance from center
+            curr_node.right.dist = curr_node.dist + 1
+            queue.append(curr_node.right)
+
+    # iterate through the dictionary storing values of node's which can be viewed from top
+    # starting from the furthest left
+    curr_dist = max_left_dist
+    while curr_dist in top_nodes_dist:
+        print(top_nodes_dist[curr_dist], end = " ")
+        curr_dist += 1
+        
+    
+
 
 
 
