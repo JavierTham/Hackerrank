bc6b702de252ba9ee223f974652e5b7df736642c
Javier Tham <javiertjl99@gmail.com>
2021-12-23 17:14:08 +0800

    halfway through BST implementation

--- Day17/BalancedBST.py
+++ Day17/BalancedBST.py
@@ -3,5 +3,20 @@ class BalancedBST:
         self.root = None
         self.height = 0
 
-    def set_root(self, node):
-        self.root = node
+    def add_node(self, node):
+        pass
+
+    def remove_node(self, node):
+        pass
+
+    def is_balanced(self):
+        pass
+
+    def balance(self):
+        pass
+
+    def get_median(self):
+        pass
+
+    
+
